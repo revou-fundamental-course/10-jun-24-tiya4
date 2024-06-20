2da10458be8cb593e7f1386bb80e6420aa5b9c99
tiya4 <nafida4401@gmail.com>
2024-06-21 00:01:42 +0700

    UPDATES index + script

--- js/script.js
+++ js/script.js
@@ -1,32 +1,21 @@
-const express = require('express');
-const app = express();
-
-// Serve static files including JavaScript
-app.use(express.static('public', {
-    setHeaders: (res, path) => {
-        if (path.endsWith('.js')) {
-            res.setHeader('Content-Type', 'text/javascript');
-        }
-    }
-}));
-
-app.listen(5500, () => {
-    console.log('Server is running on http://127.0.0.1:5500/script.js');
-});
-
 function convertTemp() {
     var inputTemp = parseFloat(document.getElementById('inputTemp').value);
-    var resultFahrenheit = (inputTemp * 9/5) + 32;
+    var inputFahrenheit = parseFloat(document.getElementById('inputFahrenheit').);
+    var resultFahrenheit = inputTemp * 9/5 + 32;
+    resultCalculate.innerHTML = `
+    Fahrenheit: ${inputTemp.toFixed(2)}°C * 9/5 + 32 = ${resultFahrenheit.toFixed(2)}°F
+`;
+}
+    
 
+
+function reverse() {
+    var inputTemp = parseFloat(document.getElementById('inputTemp').value);
+    var inputFahrenheit = parseFloat(document.getElementById('inputFahrenheit').value);
     var resultCalculate = document.getElementById('resultCalculate');
     resultCalculate.innerHTML = `
         Fahrenheit: ${inputTemp.toFixed(2)}°C * 9/5 + 32 = ${resultFahrenheit.toFixed(2)}°F
     `;
-}
-
-function reverse() {
-    var inputFahrenheit = parseFloat(document.getElementById('inputFahrenheit').value);
-    var resultCelsius = (inputFahrenheit - 32) * 5/9;
 
     var resultCalculate = document.getElementById('resultCalculate');
     resultCalculate.innerHTML = `
