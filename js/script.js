9582e91cb87a611649da877c2fa6d8cc001cb104
tiya4 <nafida4401@gmail.com>
2024-06-21 01:55:55 +0700

    updates index.html + js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,38 @@
-function convertTemp() {
-    var inputTemp = parseFloat(document.getElementById('inputTemp').value);
+function konversi() {
+    var inputTemp = parseFloat(document.getElementById('inputTemp'));
     var resultFahrenheit = inputTemp * 9/5 + 32;
 
     var resultCalculate = document.getElementById('resultFahrenheit');
-    resultCalculate.value = `
+    resultCalculate : HTMLButtonElement =
         Fahrenheit: ${inputTemp.toFixed(2)}°C * 9/5 + 32 = ${resultFahrenheit.toFixed(2)}°F
-    `;
+    ;
+    resultFahrenheit.innerhtml = inputTemp.value;
 }
 
 
 function reverse() {
-    var inputTemp = parseFloat(document.getElementById('inputTemp').value);
-    var inputFahrenheit = parseFloat(document.getElementById('inputFahrenheit').value);
-    var resultCelsius = (inputFahrenheit - 32) * 5/9; // Fixed the formula for Celsius conversion
+    var inputTemp = parseFloat(document.getElementById('inputTemp'));
+    var inputFahrenheit = parseFloat(document.getElementById('inputFahrenheit'));
+    var resultCelsius = (inputFahrenheit - 32) * 5/9;
 
     var resultCalculate = document.getElementById('resultCalculate');
-    resultCalculate.value = `
+    resultCalculate : HTMLButtonElement = 
         Fahrenheit: ${inputTemp.toFixed(2)}°C * 9/5 + 32 = ${inputFahrenheit.toFixed(2)}°F
         Celsius: (${inputFahrenheit.toFixed(2)}°F - 32) * 5/9 = ${resultCelsius.toFixed(2)}°C
-    `;
+    ;
+    resultCalculate.innerhtml = resultCelsius.value / inputFahrenheit.value
 }
 
 function reset() {
-    document.getElementById('inputTemp').value = '';
-    document.getElementById('inputFahrenheit').value = '';
-    document.getElementById('resultFahrenheit').value = ''; // Changed to 'resultFahrenheit'
-    document.getElementById('resultCalculate').value = '';
+    document.getElementById('inputTemp');
+    document.getElementById('inputFahrenheit');
+    document.getElementById('resultCalculate');
 }
 
-var button = document.getElementById('myButton');
 
-button.addEventListener('click', function() {
-    convertTemp();
-    reverse();
-    reset();
-});
+
+button.addEventListener('click', konversi);
+
+button.addEventListener('click', reset);
+
+button.addEventListener('click', reverse);
